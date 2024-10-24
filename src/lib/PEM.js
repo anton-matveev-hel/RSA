7d0788884ac6d7a6127342e68e4a4a0628a29ee5
Anton Matveev <anton.matveev@zaumstudio.com>
2024-10-25 02:39:47 +0300

    Implement PEM keys decoding

--- src/lib/PEM.js
+++ src/lib/PEM.js
@@ -2,17 +2,17 @@ const asn1js = require("asn1js");
 const util = require("./util");
 
 module.exports = {
-    encodePrivateKeyToPEM: function ( p, q, d, n, e ) {
+    encodePrivateKey: function(p, q, d, n, e) {
         const version = new asn1js.Integer( { value: 0 } );
 
-        const modulus = this._encodeASN1Integer( n );
-        const publicExponent = this._encodeASN1Integer( e );
-        const privateExponent = this._encodeASN1Integer( d );
-        const prime1 = this._encodeASN1Integer( p );
-        const prime2 = this._encodeASN1Integer( q );
-        const exponent1 = this._encodeASN1Integer( d % ( p - 1n ) );
-        const exponent2 = this._encodeASN1Integer( d % ( q - 1n ) );
-        const coefficient = this._encodeASN1Integer( util.modInverse( q, p ) );
+        const modulus = this._encodeASN1Integer(n);
+        const publicExponent = this._encodeASN1Integer(e);
+        const privateExponent = this._encodeASN1Integer(d);
+        const prime1 = this._encodeASN1Integer(p);
+        const prime2 = this._encodeASN1Integer(q);
+        const exponent1 = this._encodeASN1Integer(d % (p - 1n));
+        const exponent2 = this._encodeASN1Integer(d % (q - 1n));
+        const coefficient = this._encodeASN1Integer(util.modInverse(q, p));
 
         const sequence = this._encodeASN1Sequence( [
             version,
@@ -33,30 +33,89 @@ module.exports = {
         return pemString;
     },
 
-    encodePublicKeyToPEM: function ( n, e ) {
+    encodePublicKey: function(n, e) {
         // Encode the RSA public key as in PKCS#1
-        const modulus = this._encodeASN1Integer( n );
-        const publicExponent = this._encodeASN1Integer( e );
-        const rsaPublicKeySequence = this._encodeASN1Sequence( [ modulus, publicExponent ] );
-        const rsaPublicKeyDer = rsaPublicKeySequence.toBER( false );
+        const modulus = this._encodeASN1Integer(n);
+        const publicExponent = this._encodeASN1Integer(e);
+        const rsaPublicKeySequence = this._encodeASN1Sequence([modulus, publicExponent]);
+        const rsaPublicKeyDer = rsaPublicKeySequence.toBER(false);
 
         // AlgorithmIdentifier for RSA Encryption
-        const algorithmIdentifierSequence = this._encodeASN1Sequence( [
-            new asn1js.ObjectIdentifier( { value: '1.2.840.113549.1.1.1' } ), // rsaEncryption OID
+        const algorithmIdentifierSequence = this._encodeASN1Sequence([
+            new asn1js.ObjectIdentifier({ value: '1.2.840.113549.1.1.1' }), // rsaEncryption OID
             new asn1js.Null(), // Parameters are NULL for rsaEncryption
-        ] );
+        ]);
 
         // SubjectPublicKeyInfo
-        const subjectPublicKeyInfoSequence = this._encodeASN1Sequence( [
+        const subjectPublicKeyInfoSequence = this._encodeASN1Sequence([
             algorithmIdentifierSequence,
             new asn1js.BitString( { valueHex: rsaPublicKeyDer } ),
-        ] );
+        ]);
 
-        const derEncoded = subjectPublicKeyInfoSequence.toBER( false );
-        const base64String = this._arrayBufferToBase64( derEncoded );
-        const pemString = this._wrapPEM( base64String, 'PUBLIC KEY' );
+        const derEncoded = subjectPublicKeyInfoSequence.toBER(false);
+        const base64String = this._arrayBufferToBase64(derEncoded);
+        return this._wrapPEM(base64String, 'PUBLIC KEY');
+    },
 
-        return pemString;
+    decodePrivateKey: function(pemString) {
+        let base64;
+        try {
+            base64 = this._unwrapPEM(pemString, "RSA PRIVATE KEY")
+        } catch(e) {
+            throw("Incorrect private key format.");
+        }
+        const arrayBuffer = this._base64ToArrayBuffer(base64);
+        const asn1 = asn1js.fromBER(arrayBuffer);
+        if (asn1.offset === -1) {
+            throw new Error('Error parsing ASN.1 structure');
+        }
+
+        // RSAPrivateKey structure
+        const rsaPrivateKey = asn1.result;
+
+        const modulus = rsaPrivateKey.valueBlock.value[1].valueBlock.valueHex;
+        const publicExponent = rsaPrivateKey.valueBlock.value[2].valueBlock.valueHex;
+        const privateExponent = rsaPrivateKey.valueBlock.value[3].valueBlock.valueHex;
+
+        return {
+            n: BigInt("0x" + this._bufferToHex(modulus)),
+            e: BigInt("0x" + this._bufferToHex(publicExponent)),
+            d: BigInt("0x" + this._bufferToHex(privateExponent)),
+        };
+    },
+
+    decodePublicKey: function(pemString) {
+        let base64;
+        try {
+            base64 = this._unwrapPEM(pemString,"PUBLIC KEY")
+        } catch(e) {
+            throw("Incorrect public key format.");
+        }
+        const arrayBuffer = this._base64ToArrayBuffer(base64);
+        const asn1 = asn1js.fromBER(arrayBuffer);
+        if (asn1.offset === -1) {
+            throw new Error('Error parsing ASN.1 structure');
+        }
+
+        let modulus, publicExponent;
+
+        const subjectPublicKeyInfo = asn1.result;
+        const bitString = subjectPublicKeyInfo.valueBlock.value[1];
+        if (bitString.idBlock.tagClass !== 1 || bitString.idBlock.tagNumber !== 3) {
+            throw new Error('Expected a BIT STRING for public key data');
+        }
+        const rsaPublicKeyASN1 = bitString.valueBlock.value[0];
+        if (rsaPublicKeyASN1.offset === -1) {
+            throw new Error('Error parsing RSA public key');
+        }
+        const rsaPublicKey = rsaPublicKeyASN1.valueBlock;
+        modulus = rsaPublicKey.value[0].valueBlock.valueHex;
+        publicExponent = rsaPublicKey.value[1].valueBlock.valueHex;
+
+        return {
+            n: BigInt("0x" + this._bufferToHex(modulus)),
+            e: BigInt("0x" + this._bufferToHex(publicExponent)),
+        };
     },
 
     _encodeASN1Integer: function ( bigIntValue ) {
@@ -91,7 +150,7 @@ module.exports = {
         return btoa( binary );
     },
 
-    _wrapPEM: function ( base64String, label ) {
+    _wrapPEM: function(base64String, label) {
         const pemHeader = `-----BEGIN ${label}-----\n`;
         const pemFooter = `\n-----END ${label}-----\n`;
 
@@ -100,5 +159,28 @@ module.exports = {
         const lines = base64String.match( new RegExp( '.{1,' + lineLength + '}', 'g' ) );
 
         return pemHeader + lines.join( '\n' ) + pemFooter;
+    },
+
+    _unwrapPEM: function(pemString, label) {
+        return pemString
+            .replace(`-----BEGIN ${label}-----`, '')
+            .replace(`-----END ${label}-----`, '')
+            .replace(/\s+/g, '');
+    },
+
+    _base64ToArrayBuffer: function(base64) {
+        // In Node.js
+        const buffer = Buffer.from(base64, 'base64');
+        return buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+    },
+
+    _bufferToHex: function(arrayBuffer) {
+        const byteArray = new Uint8Array(arrayBuffer);
+        let hexString = '';
+        for (let i = 0; i < byteArray.length; i++) {
+            const hex = byteArray[i].toString(16).padStart(2, '0');
+            hexString += hex;
+        }
+        return hexString;
     }
 };
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,11 @@
 const rsa = require("./RSA");
 const pem = require("./PEM");
 const fs = require("fs");
+const yargs = require("yargs");
 
-const keys = rsa.generateKeys(2048);
-const { p, q, d, n, e } = keys;
-console.log( keys );
-// const publicPem = pem.encodePublicKeyToPEM(n, e);
-// fs.writeFileSync("pub_key.pem", publicPem);
-// const privatePem = pem.encodePrivateKeyToPEM(p, q, d, n, e);
-// fs.writeFileSync("key.pem", privatePem);
+
+const options = yargs
+    .usage(usage)
+    .option("l", {alias:"languages", describe: "List all supported languages.", type: "boolean", demandOption
+            : false })
+    .help(true).argv;
